d38127498732d0236f997656cf05fdc1346ec7ac
monsalvechs <monsalvechs@gmail.com>
2021-08-16 21:30:31 -0300

    cambios
    pautas 1 y 2 realizadas, de la entrega 1

--- js/products.js
+++ js/products.js
@@ -3,4 +3,33 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
 
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            loadProducts(resultObj.data);
+        }
+    })
+
+    function loadProducts (arregloProducto) {
+        let contenidoHtml = "";
+        for(let i = 0; i < arregloProducto.length; i++){
+            let product = arregloProducto[i];
+                contenidoHtml += `
+                <a href="product-info.html" class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">`+ product.name +`</h4>
+                                <small class="text-muted">` + product.productCount + ` artículos</small>
+                            </div>
+                            <p class="mb-1">` + product.description + `</p>
+                        </div>
+                    </div>
+                </a>
+                `
+            document.getElementById("prod-list-container").innerHTML = contenidoHtml;
+        }
+    }
 });
