a1df6f0f59acb8bbd2d45be8fd83d5572ed6f77a
monsalvechs <monsalvechs@gmail.com>
2021-08-25 14:24:09 -0300

    ca

--- js/login.js
+++ js/login.js
@@ -1,11 +1,24 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-
-});
-
-
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("formulario").addEventListener('submit', validarFormulario); 
+  });
+  
+  function validarFormulario(evento) {
+    evento.preventDefault();
+    var email = document.getElementById('inputEmail').value;
+    if(email.length == 0) {
+      alert('No has escrito nada en correo');
+      return;
+    }
+    var password = document.getElementById('inputPassword').value;
+    if (password.length < 6) {
+      alert('La clave no es válida');
+      return;
+    }
+    this.submit();
+  }
 /*var element = document.getElementsById('containerp-5-1'){
 }*/
 
