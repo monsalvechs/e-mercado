e27a0d5c2e0648dc3d95a490e8ee45b42b2f8247
monsalvechs <monsalvechs@gmail.com>
2021-09-02 14:52:42 -0300

    cambios 02/09

--- js/products.js
+++ js/products.js
@@ -1,6 +1,14 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+var minCount = undefined;
+    var maxCount = undefined;
+if (((minCount == undefined) || (minCount != undefined && parseInt(loadProduct.productCount) >= minCount)) &&
+            ((maxCount == undefined) || (maxCount != undefined && parseInt(loadProduct.productCount) <= maxCount)))
+             
+
+
+
 document.addEventListener("DOMContentLoaded", function (e) {
 
  getJSONData(PRODUCTS_URL).then (function (objResult) {
@@ -9,11 +17,12 @@ document.addEventListener("DOMContentLoaded", function (e) {
         }
     })
 
+
     function loadProducts (arregloProductoHtml) {
         let contenidoHtml = "";
         for(let i = 0; i < arregloProductoHtml.length; i++){
-
             let product = arregloProductoHtml[i];
+
                 contenidoHtml += `
                 <a href="product-info.html" class="list-group-item list-group-item-action">
                     <div class="row">
@@ -31,6 +40,8 @@ document.addEventListener("DOMContentLoaded", function (e) {
                     </div>
                 </a>
                 `
+            
+
             document.getElementById("prod-list-container").innerHTML = contenidoHtml;
         }
     }
